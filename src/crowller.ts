e715b2f4c4240c16fd77151e672a88cd315bf3de
Lisa Huang <LisaHuangAde19@gmail.com>
2022-04-01 11:40:05 +1030

    write file done, to be optimised

--- src/crowller.ts
+++ src/crowller.ts
@@ -2,18 +2,26 @@
 
 import superagent from 'superagent'
 import cheerio from 'cheerio'
-
+import fs from 'fs'
+import path from 'path'
 
 interface Course {
     title: string,
     count: number
 }
+interface CourseResult {
+    time: number,
+    data: Course[]
+}
+interface Content {
+    [propName: number]: Course[]
+}
+
 
 class Crowller {
     private secret = 'secretKey'
     private url = `http://www.dell-lee.com/typescript/demo.html?secret=${this.secret}`
-
-    private rawHtml = ''
+    private filePath = path.resolve(__dirname, '../data/course.json')
 
     getCouseInfo = (html: string) => {
         const $ = cheerio.load(html)
@@ -23,22 +31,45 @@ class Crowller {
             const descs = $(element).find('.course-desc')
             const title = descs.eq(0).text()
             const count = parseInt(descs.eq(1).text().split('：')[1])
-            console.log(title, count)
             courseInfos.push({ title, count })
         })
-        const result = {
+        return {
             time: (new Date()).getTime(),
             data: courseInfos
         }
     }
 
-    async getRawHteml() {
+    async getRawHtml() {
         const result = await superagent.get(this.url)
-        this.getCouseInfo(result.text)
+        return result.text
     }
 
+
+    generateJsonContent(courseInfo: CourseResult) {
+        let fileContent: Content = {}
+
+        // get the existing course info already in the file
+        if (fs.existsSync(this.filePath)) {
+            fileContent = JSON.parse(fs.readFileSync(this.filePath, 'utf-8'))
+        }
+        // + new info - courseInfo
+        fileContent[courseInfo.time] = courseInfo.data
+
+        return fileContent;
+    }
+
+    writeFile(content: string) {
+        fs.writeFileSync(this.filePath, content)
+    }
+
+    async initSpiderProcess() {
+        const html = await this.getRawHtml()
+        const courseInfo = this.getCouseInfo(html)
+        const fileContent = this.generateJsonContent(courseInfo)
+        this.writeFile(JSON.stringify(fileContent))
+    }
     constructor() {
-        this.getRawHteml();
+        this.initSpiderProcess();
     }
 }
 
