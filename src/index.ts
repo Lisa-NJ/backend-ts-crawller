c76f74371f7987c37fe87a88b31681d5bab6da94
Lisa Huang <LisaHuangAde19@gmail.com>
2022-04-02 07:33:40 +1030

    introduce body-parser to fix errors

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express'
 import router from './router'
+// var bodyParser = require('body-parser') // commonJS 语法
+import bodyParser from 'body-parser' // ts 支持的 import
 
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(router)
 
 app.listen(7001, () => {
--- src/router.ts
+++ src/router.ts
@@ -5,18 +5,30 @@ import Crawller from './crawller'
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-    res.send('Hello, I am express')
+    res.send(`
+    <html>
+        <body>
+            <form method='post' action='/getData'>
+            <input type='password' name='password'>
+            <button>submit</button>
+        </body>
+    </html>
+    `)
 })
 
-router.get('/getData', (req: Request, res: Response) => {
+router.post('/getData', (req: Request, res: Response) => {
+    const { password } = req.body
+    if (password === '123') {
+        const secret = 'secretKey'
+        const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
 
-    const secret = 'secretKey'
-    const url = `http://www.dell-lee.com/typescript/demo.html?secret=${secret}`
+        const analyzer = DellAnalyzer.getInstance()
+        new Crawller(url, analyzer)
 
-    const analyzer = DellAnalyzer.getInstance()
-    new Crawller(url, analyzer)
-
-    res.send('getDate success')
+        res.send('getDate success!')
+    } else {
+        res.send('password Error!')
+    }
 })
 
 export default router
