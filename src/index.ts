e81f58846cb1e966c0d2a10e45fdaaf305746421
Lisa Huang <LisaHuangAde19@gmail.com>
2022-04-02 21:09:38 +1030

    add Router Logout

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,23 @@
 import express, { Request, Response, NextFunction } from 'express'
-import router from './router'
 // var bodyParser = require('body-parser') // commonJS 语法
 import bodyParser from 'body-parser' // ts 支持的 import
+import cookieSession from 'cookie-session'
+import router from './router'
 
 const app = express()
+
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use((req: Request, res: Response, next: NextFunction) => {
-    req.teacherName = 'dell'
-    next()
-})
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['teacher dell'],
+
+    // Cookie Options
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
+
 app.use(router)
 
 app.listen(7001, () => {
     console.log('which server is running on 7001?')
-})
+})    
--- src/router.ts
+++ src/router.ts
@@ -11,15 +11,52 @@ interface RequestWithBody extends Request {
 const router = Router()
 
 router.get('/', (req: Request, res: Response) => {
-    res.send(`
-    <html>
-        <body>
-            <form method='post' action='/getData'>
-            <input type='password' name='password'>
-            <button>submit</button>
-        </body>
-    </html>
-    `)
+    const isLogin = req.session ? req.session.login : undefined
+
+    if (isLogin) {
+        res.send(`
+        <html>
+            <body>
+               <a href='/logout'>Logout</a>
+            </body>
+        </html>
+      `)
+    } else {
+        res.send(`
+            <html>
+                <body>
+                    <form method='post' action='/login'>
+                    <input type='password' name='password'>
+                    <button>Login</button>
+                </body>
+            </html>
+          `)
+    }
+})
+
+router.get('/logout', (req: Request, res: Response) => {
+    console.log('router--logout')
+
+    if (req.session) {
+        req.session.login = undefined
+    }
+    res.redirect('/')
+})
+
+router.post('/login', (req: RequestWithBody, res: Response) => {
+    const { password } = req.body
+    const isLogin = req.session ? req.session.login : undefined
+    if (isLogin) {
+        res.send('already logined in')
+    } else {
+        if (password === '123' && req.session) {
+            req.session.login = true
+            res.send('login success!')
+        } else {
+            res.send('login fail!')
+        }
+    }
+
 })
 
 router.post('/getData', (req: RequestWithBody, res: Response) => {
@@ -33,7 +70,7 @@ router.post('/getData', (req: RequestWithBody, res: Response) => {
 
         res.send('getDate success!')
     } else {
-        res.send(`${req.teacherName} password Error!`)
+        res.send('password Error!')
     }
 })
 
